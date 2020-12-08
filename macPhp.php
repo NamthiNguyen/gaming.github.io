8a43165b6462736512df6863eb1f5e44ef812698
namthi267 <70618347+namthi267@users.noreply.github.com>
2020-12-08 00:53:13 -0500

    Add files via upload

---
+++ macPhp.php
@@ -0,0 +1,8 @@
+<?php
+$word1 = $_REQUEST["word1"];
+$word2 = $_REQUEST["word2"];
+
+    $command = escapeshellcmd("python3 /var/www/html/machineLearning/useMacLearn.py $word1 $word2");
+    $output = shell_exec($command);
+    echo $output;
+?>
